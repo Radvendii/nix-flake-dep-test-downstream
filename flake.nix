e6f0d5790f3736a2b7e85b648cecc49f675f63d8
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-09-13 11:00:11 -0400

    updated upstream input

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
-  inputs.upstream.url = "github:radvendii/nix-flake-dep-test-upstream/27ffa18b5a130873796bb06b8a778b522814774a";
-  # inputs.upstream.url = "github:radvendii/nix-flake-dep-test-upstream/57c136eb8aacc972553bae8562faf6ac277d5435";
+  # inputs.upstream.url = "github:radvendii/nix-flake-dep-test-upstream/27ffa18b5a130873796bb06b8a778b522814774a";
+  inputs.upstream.url = "github:radvendii/nix-flake-dep-test-upstream/57c136eb8aacc972553bae8562faf6ac277d5435";
   outputs = { self, nixpkgs, upstream }: {
     packages.x86_64-linux.default = upstream.packages.x86_64-linux.default;
   };
